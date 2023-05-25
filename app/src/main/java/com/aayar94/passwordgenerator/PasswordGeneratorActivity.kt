139925b3b6e0f8ee157475ca9996e211bdc66875
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-05-25 22:46:24 +0300

    Copy button icon added & Extented material icon library added to gradle

--- app/src/main/java/com/aayar94/passwordgenerator/PasswordGeneratorActivity.kt
+++ app/src/main/java/com/aayar94/passwordgenerator/PasswordGeneratorActivity.kt
@@ -18,9 +18,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -71,12 +75,15 @@ fun PasswordGenerator() {
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                stringResource(id = R.string.app_name),
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                style = MaterialTheme.typography.headlineLarge
+                stringResource(id = R.string.generate_a_password),
+                modifier = Modifier.align(CenterHorizontally),
+                style = MaterialTheme.typography.headlineSmall
             )
             Spacer(modifier = Modifier.height(10.dp))
-            Column(verticalArrangement = Arrangement.Center) {
+
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
@@ -88,86 +95,96 @@ fun PasswordGenerator() {
                     Spacer(modifier = Modifier.width(5.dp))
                     Button(
                         onClick = {
-                            val clipboardManager =
-                                getSystemService(
-                                    context,
-                                    ClipboardManager::class.java
-                                ) as ClipboardManager
+                            val clipboardManager = getSystemService(
+                                context,
+                                ClipboardManager::class.java
+                            ) as ClipboardManager
                             val clipData = ClipData.newPlainText("text", generatedPassword)
                             clipboardManager.setPrimaryClip(clipData)
 
-                            Toast.makeText(context, "Password Copied", Toast.LENGTH_LONG).show()
+                            Toast.makeText(context, "Password Copied", Toast.LENGTH_SHORT).show()
                         },
-                        enabled = generatedPassword.isNotEmpty(), shape = RoundedCornerShape(5.dp)
+                        enabled = generatedPassword.isNotEmpty(),
+                        shape = RoundedCornerShape(15.dp)
                     ) {
+                        Icon(
+                            imageVector = Icons.Default.ContentCopy,
+                            contentDescription = stringResource(
+                                id = R.string.copy
+                            )
+                        )
                         Text(stringResource(id = R.string.copy))
                     }
                 }
-                Spacer(modifier = Modifier.height(10.dp))
-                LabeledCheckbox(
-                    label = stringResource(id = R.string.upper_case),
-                    onCheckChange = {
-                        isUpper = !isUpper
-                    },
-                    isChecked = isUpper
-                )
-                LabeledCheckbox(
-                    label = stringResource(id = R.string.lower_case),
-                    onCheckChange = {
-                        isLower = !isLower
-                    },
-                    isChecked = isLower
-                )
-                LabeledCheckbox(
-                    label = stringResource(id = R.string.numeric),
-                    onCheckChange = {
-                        isNumeric = !isNumeric
-                    },
-                    isChecked = isNumeric
-                )
-                EditableCheckbox(
-                    value = customPasswordSetting,
-                    onCheckChange = {
-                        if (!customPasswordSetting.isNotEmpty()) {
-                            isCustom = !isCustom
-                        }
-                    },
-                    onValueChanged = {
-                        if (it.isEmpty()) {
-                            isCustom = false
-                        }
-                        customPasswordSetting = it
-                    },
-                    isChecked = isCustom
-                )
-                PasswordSize(
-                    passwordSize = passwordSize,
-                    onValueChange = {
-                        if ((passwordSize.isNotEmpty() && passwordSize.toInt() < 200) || it.isEmpty()) {
-                            passwordSize = it
-                        }
-                    }
-                )
-
-                Button(
-                    shape = RoundedCornerShape(5.dp),
-                    onClick = {
-                        val pwdGenerator = PasswordGenerator.Builder()
-                            .addUpper(isUpper)
-                            .addLower(isLower)
-                            .addNumeric(isNumeric)
-                            .addCustom(isCustom, CustomPwdContent(customPasswordSetting))
-                            .setSize(if (passwordSize.isEmpty()) 8 else passwordSize.toInt())
-                            .build()
-
-                        generatedPassword = pwdGenerator.generatePassword()
-                    },
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = (isUpper || isLower || isCustom || isNumeric) && passwordSize.isNotEmpty() && passwordSize.toInt() > 0
-                ) {
-                    Text(text = stringResource(id = R.string.generate))
-                }
             }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            LabeledCheckbox(
+                label = stringResource(id = R.string.upper_case),
+                onCheckChange = {
+                    isUpper = !isUpper
+                },
+                isChecked = isUpper
+            )
+            LabeledCheckbox(
+                label = stringResource(id = R.string.lower_case),
+                onCheckChange = {
+                    isLower = !isLower
+                },
+                isChecked = isLower
+            )
+            LabeledCheckbox(
+                label = stringResource(id = R.string.numeric),
+                onCheckChange = {
+                    isNumeric = !isNumeric
+                },
+                isChecked = isNumeric
+            )
+            EditableCheckBox(
+                value = customPasswordSetting,
+                onCheckChange = {
+                    if (customPasswordSetting.isNotEmpty())
+                        isCustom = !isCustom
+                },
+                isChecked = isCustom,
+                onValueChange = {
+                    if (it.isEmpty()) {
+                        isCustom = false
+                    }
+                    customPasswordSetting = it
+                }
+            )
+
+            PasswordSize(
+                passwordSize = passwordSize,
+                onValueChange = {
+                    if ((it.isNotEmpty() && it.toInt() < 200) || it.isEmpty()) {
+                        passwordSize = it
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                shape = RoundedCornerShape(5.dp),
+                onClick = {
+                    val pwdGen = PasswordGenerator.Builder()
+                        .addUpper(isUpper)
+                        .addLower(isLower)
+                        .addNumeric(isNumeric)
+                        .addCustom(isCustom, CustomPwdContent(customPasswordSetting))
+                        .setSize(if (passwordSize.isEmpty()) 8 else passwordSize.toInt())
+                        .build()
+
+                    generatedPassword = pwdGen.generatePassword()
+
+                },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = (isUpper || isLower || isCustom || isNumeric) && passwordSize.isNotEmpty() && passwordSize.toInt() > 0
+            ) {
+                Text(text = stringResource(id = R.string.generate))
+            }
+
         }
     }
 }
@@ -175,7 +192,10 @@ fun PasswordGenerator() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PasswordSize(passwordSize: String, onValueChange: (String) -> Unit) {
-    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Text(
             text = stringResource(id = R.string.password_size),
             modifier = Modifier.weight(1f)
@@ -185,7 +205,7 @@ fun PasswordSize(passwordSize: String, onValueChange: (String) -> Unit) {
             onValueChange = onValueChange,
             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
             placeholder = { Text("Max 200") },
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.weight(1f)
         )
     }
 }
@@ -205,21 +225,23 @@ fun LabeledCheckbox(label: String, onCheckChange: () -> Unit, isChecked: Boolean
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditableCheckbox(
+fun EditableCheckBox(
     value: String,
     onCheckChange: () -> Unit,
-    onValueChanged: (String) -> Unit,
+    onValueChange: (String) -> Unit,
     isChecked: Boolean,
 ) {
     Row(
         modifier = Modifier
-            .clickable(onClick = onCheckChange)
+            .clickable(
+                onClick = onCheckChange
+            )
             .fillMaxWidth()
             .padding(vertical = 5.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(checked = isChecked, onCheckedChange = null)
-        TextField(value = value, onValueChange = onValueChanged)
+        TextField(value = value, onValueChange = onValueChange)
     }
 }
 
