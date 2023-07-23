79c78021a230b974fe3e09019988ba52b181526c
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-07-23 15:35:46 +0300

    created Constant class for handling static values

---
+++ app/src/main/java/com/aayar94/passwordgenerator/common/Constant.kt
@@ -0,0 +1,9 @@
+package com.aayar94.passwordgenerator.common
+
+class Constant {
+    companion object {
+
+        const val DATABASE_NAME = "saved_passwords_database"
+        const val PASSWORDS_TABLE_NAME = "saved_passwords_table"
+    }
+}
